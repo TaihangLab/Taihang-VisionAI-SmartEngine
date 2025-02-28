c62faadba05a524a85f24cbc7a39965f8adb4968
zhangyemao <867296114@qq.com>
2025-02-28 10:31:02 +0800

    更新代码

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -1,19 +1,39 @@
-class AIEngineError(Exception):
+class BaseError(Exception):
     """基础异常类"""
     pass
 
-class ModelError(AIEngineError):
-    """模型相关错误"""
-    pass
-
-class SkillError(AIEngineError):
-    """技能相关错误"""
-    pass
-
-class ConfigError(AIEngineError):
+class ConfigError(BaseError):
     """配置相关错误"""
     pass
 
-class MessageError(AIEngineError):
+class VideoProcessError(BaseError):
+    """视频处理相关错误"""
+    pass
+
+class ModelError(BaseError):
+    """模型相关错误"""
+    pass
+
+class StorageError(BaseError):
+    """存储相关错误"""
+    pass
+
+class AuthenticationError(BaseError):
+    """认证相关错误"""
+    pass
+
+class RateLimitError(BaseError):
+    """限流相关错误"""
+    pass
+
+class TaskError(BaseError):
+    """任务处理相关错误"""
+    pass
+
+class SkillError(BaseError):
+    """技能相关错误"""
+    pass
+
+class MessageError(BaseError):
     """消息队列相关错误"""
     pass 
